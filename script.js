356ea8898552761fee1358904e27dcef2928b7ec
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2022-10-13 15:39:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function DA() {
+	alert("Desktop Apps - Apps run on Desktop computers and Laptops");
+}
+
+function WA() {
+	alert("Web Apps - Apps run on Web Browser. ");
+}
+
+function MA() {
+	alert("Mobile Apps - Apps run in mobiles, tablets, watches, etc.  ");
+}
